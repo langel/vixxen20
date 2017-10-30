77447edb009d04e2144200c3ee97b4c54fffd19f
Langel <knoxburry@gmail.com>
2017-10-30 04:24:44 -0500

    running on electron now

---
+++ index.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+var win;
+
+function init_window() {
+	win = new BrowserWindow({
+		width: 1200,
+		height: 800
+	});
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	win.webContents.openDevTools();
+	win.on('closed', () => {
+		win = null;
+	});
+}
+
+app.on('ready', init_window);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (win === null) init_window();
+});
+
+
