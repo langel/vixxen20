c9e771feb24ac65adc8f5f46c30815b6191f95d0
langel <knoxburry@gmail.com>
2018-07-09 13:28:04 -0400

    customized app menu

---
+++ compile-osx.sh
@@ -0,0 +1,2 @@
+electron-packager . --electron-version=2.0.4
+vixxen20-darwin-x64/vixxen20.app/Contents/MacOS/vixxen20 
--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const path = require('path');
 const url = require('url');
 
 var win;
 
+const menus = [
+	{
+		label: 'ViXXen 20',
+		submenu: [
+			{role: 'quit'}
+		]
+	},
+	{
+		label: 'Edit',
+		submenu: [
+			{role: 'undo'},
+			{role: 'redo'},
+			{type: 'separator'},
+			{role: 'cut'},
+			{role: 'copy'},
+			{role: 'paste'},
+			{role: 'delete'},
+			{role: 'selectall'}
+		]
+	},
+	{
+		role: 'window',
+		submenu: [
+			{
+				label: 'Reboot',
+				click() { alert('ass'); }
+			},
+			{role: 'minimize'},
+			{role: 'close'}
+		]
+	},
+	{
+		role: 'help',
+		submenu: [
+			{
+				label: 'Learn More',
+				click () { require('electron').shell.openExternal('https://electronjs.org') }
+			}
+		]
+	}
+];
+
 function init_window() {
+	// setup app menus
+	const menu = Menu.buildFromTemplate(menus);
+	Menu.setApplicationMenu(menu);
+	// set window size
 	win = new BrowserWindow({
 		width: 1200,
 		height: 800
 	});
+	// load application
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file:',
